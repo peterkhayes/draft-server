a2808b9bdb9a0c2a4e0d975be14e67dbb492e987
Peter Hayes <peter.k.hayes@gmail.com>
2018-10-30 00:36:12 -0700

    fixed some stuff

--- client/src/PlayerCards.js
+++ client/src/PlayerCards.js
@@ -10,7 +10,7 @@ type Props = {
 export default function PlayerCards({cards}: Props) {
   return (
     <div>
-      {cards.length > 0 && <h3>You've picked these cards</h3>}
+      {cards.length > 0 && <h3>You've picked these {cards.length} cards</h3>}
       <ul>
         {cards.map((card) => <li key={card}>{card}</li>)}
       </ul>
